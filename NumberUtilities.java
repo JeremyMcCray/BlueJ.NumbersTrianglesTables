a0d58e627b394730100eddd8d57512a60b638b92
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-05 20:12:41 -0500

    halfway done

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,64 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+      String solu = "";
+      for(int i=0; i<stop;i++){
+        solu+= i;
+      }
+
+        return solu;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+      String solu = "";
+      for(int i = start; i <stop;i++){
+        solu+=i;
+      }
+        return solu;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+      String solu = "";
+      for(int i = start; i <stop;i+=step){
+        solu+=i;
+
+      }
+       return solu;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+      String solu = "";
+      for(int i = start;i<stop;i++){
+        if(i%2==0){
+          solu+=i;
+        }
+      }
+        return solu;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+      String solu = "";
+      for(int i = start;i<stop;i++){
+        if(i%2==1){
+          solu+=i;
+        }
+      }
+        return solu;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+      String solu = "";
+      int temp = 0;
+      for(int i = start;i<=stop;i++){
+        temp = (int)Math.pow(i,exponent);
+
+        solu+=temp;
+        temp=0;
+        }
+         return solu;
+      }
+
     }
-}
